5d01aa3745bbb1cbfab0202cf997b9d50baae86b
Justin Teats <justinteats@Justins-MacBook-Air.local>
2022-07-05 18:21:45 -0700

    replaced variables in js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,61 +1,19 @@
-var moment = require('moment'); // require
-moment().format(); 
+const moment = require("moment");
 
-$(document).ready(function() 
-{
-    $('.saveBtn').on('click', function() {
-      var value = $(this)
-        .siblings('.description')
-        .val();
-      var time = $(this)
-        .parent()
-        .attr('id');
-  
-      localStorage.setItem(time, value);
-  
-      $('.notification').addClass('show');
-  
-      setTimeout(function() {
-        $('.notification').removeClass('show');
-      }, 5000);
-    });
-  
-    function hourUpdater() {
-      var currentHour = moment().hours();
-  
-      $('.time-block').each(function() {
-        var blockHour = parseInt(
-          $(this)
-            .attr('id')
-            .split('-')[1]
-        );
-  
- if (blockHour < currentHour) {
-          $(this).addClass('past')
-    } else if (blockHour === currentHour) {
-          $(this).removeClass('past');
-          $(this).addClass('present');
-    } else {
-          $(this).removeClass('past');
-          $(this).removeClass('present');
-          $(this).addClass('future');
-        }
-      });
-    }
-  
-    hourUpdater();
-  
-    var interval = setInterval(hourUpdater, 15000);
-  
-    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
-  
-    $('#currentDay').text(moment().format('dddd, MMMM Do'));
-  });
+//date in header
+var currentDay = $("#currentDay");
+currentDate = moment(currentData).format("dddd, MMMM Do");
+
+var header = $("header");
+header.append(currentDate);
+
+var now = moment();
+var currentTime = {text: moment().format("h:00 A"), hour: moment().hour() };
+
+//arrays for each hour
+var hoursOfDay =Array.from(new Array(9)).map((v, i) => {
+  var text = moment().hour(i).format("hA");
+});
+
+var textDescrip = $(this).siblings(".description").val();
+var time = $(this).parent().Attr("id");
